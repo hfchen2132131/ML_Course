27cb97abd5f9f6eec21973dcf1982fd9ac49e669
hfchen2132131 <58462029+hfchen2132131@users.noreply.github.com>
2020-03-19 17:05:45 +0800

    improve

--- ml_play.py
+++ ml_play.py
@@ -22,6 +22,7 @@ def ml_loop():
     # === Here is the execution order of the loop === #
     # 1. Put the initialization code here.
     ball_served = False
+    pre_ball_posi = (0,0)
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
@@ -30,26 +31,43 @@ def ml_loop():
     while True:
         # 3.1. Receive the scene information sent from the game process.
         scene_info = comm.get_scene_info()
-
+        isDrop = False
         # 3.2. If the game is over or passed, the game process will reset
         #      the scene and wait for ml process doing resetting job.
         if scene_info.status == GameStatus.GAME_OVER or \
             scene_info.status == GameStatus.GAME_PASS:
             # Do some stuff if needed
             ball_served = False
+            pre_ball_posi = (0,0)
 
             # 3.2.1. Inform the game process that ml process is ready
             comm.ml_ready()
             continue
 
         # 3.3. Put the code here to handle the scene information
-
+        delta = (scene_info.ball[0] - pre_ball_posi[0] , scene_info.ball[1] - pre_ball_posi[1])
+        drop = 0
+        if delta[1] > 0:
+            isDrop = True
+            drop = scene_info.ball[0] + delta[0] * (400 - scene_info.ball[1]) / delta[1]
+            drop = int(drop)
+            drop = drop - (drop % 5)
+                
+            if drop > 275 or drop < -75:
+                isDrop = False
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            if scene_info.ball[0] > scene_info.platform[0]:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+            if isDrop:
+                if drop > scene_info.platform[0] + 20:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                elif drop < scene_info.platform[0] + 20:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
             else:
-                comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+                if 100 > scene_info.platform[0] + 20:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                elif 100 < scene_info.platform[0] + 20:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+        pre_ball_posi = scene_info.ball
