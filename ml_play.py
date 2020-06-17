b76b3b51ac6b1492521eb9e630b7aefef7d8db82
hfchen2132131 <58462029+hfchen2132131@users.noreply.github.com>
2020-06-17 22:09:27 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -1,134 +1,85 @@
-"""
-The template of the main script of the machine learning process
-"""
-import pickle
-import numpy as np
-import random
-import sklearn.svm
+import math
 
-from mlgame.communication import ml as comm
-import os.path as path
+class MLPlay:
+    def __init__(self, player):
+        self.player = player
+        if self.player == "player1":
+            self.player_no = 0
+        elif self.player == "player2":
+            self.player_no = 1
+        elif self.player == "player3":
+            self.player_no = 2
+        elif self.player == "player4":
+            self.player_no = 3
+        self.car_vel = 0
+        self.car_pos = ()
+        self.direction = -1;
+        pass
 
-
-def ml_loop(side: str):
-    """
-    The main loop of the machine learning process
-
-    This loop is run in a separate process, and communicates with the game process.
-
-    Note that the game process won't wait for the ml process to generate the
-    GameInstruction. It is possible that the frame of the GameInstruction
-    is behind of the current frame in the game process. Try to decrease the fps
-    to avoid this situation.
-    """
-
-    # === Here is the execution order of the loop === #
-    # 1. Put the initialization code here.
-    ball_served = False
-    if side == "1P":
-        filename = path.join(path.dirname(__file__),"clf_KMeans_PingPong.pickle")
-    else:
-        filename = path.join(path.dirname(__file__),"clf_KMeans_PingPong_p2.pickle")
-    with open(filename, 'rb') as file:
-        clf_p1 = pickle.load(file)
-    
-
-    s = [93,93]
-    def get_direction(ball_x,ball_y,ball_pre_x,ball_pre_y):
-        VectorX = ball_x - ball_pre_x
-        VectorY = ball_y - ball_pre_y
-        if(VectorX>=0 and VectorY>=0):
-            return 0
-        elif(VectorX>0 and VectorY<0):
-            return 1
-        elif(VectorX<0 and VectorY>0):
-            return 2
-        elif(VectorX<0 and VectorY<0):
-            return 3
-
-    # 2. Inform the game process that ml process is ready before start the loop.
-    comm.ml_ready()
-    
-    
-        
-
-    # 3. Start an endless loop.
-    while True:
-        # 3.1. Receive the scene information sent from the game process.
-        scene_info = comm.recv_from_game()
-        feature = []
-        if side == '1P':
-            feature.append(scene_info["platform_1P"][0])
-        else:
-            feature.append(scene_info["platform_2P"][0])
-        feature.append(scene_info["blocker"][0])
-        feature.append(scene_info["ball_speed"][0])
-        feature.append(scene_info["ball_speed"][1])
-        if side == '1P':
-            if scene_info["ball_speed"][1] > 0 : # 球正在向下 # ball goes down
-                x = ( scene_info["platform_1P"][1]-scene_info["ball"][1] ) // scene_info["ball_speed"][1] # 幾個frame以後會需要接  # x means how many frames before catch the ball
-                pred = scene_info["ball"][0]+(scene_info["ball_speed"][0]*x)  # 預測最終位置 # pred means predict ball landing site 
-                bound = pred // 200 # Determine if it is beyond the boundary
-                if (bound > 0): # pred > 200 # fix landing position
-                    if (bound%2 == 0) : 
-                        pred = pred - bound*200                    
-                    else :
-                        pred = 200 - (pred - 200*bound)
-                elif (bound < 0) : # pred < 0
-                    if (bound%2 ==1) :
-                        pred = abs(pred - (bound+1) *200)
-                    else :
-                        pred = pred + (abs(bound)*200)
-            else : # 球正在向上 # ball goes up
-                pred = 100
-        elif side == '2P':
-            if scene_info["ball_speed"][1] < 0 : # 球正在向下 # ball goes down
-                x = ( scene_info["platform_2P"][1]+30-scene_info["ball"][1] ) // scene_info["ball_speed"][1] 
-                pred = scene_info["ball"][0]+(scene_info["ball_speed"][0]*x) 
-                bound = pred // 200 
-                if (bound > 0):
-                    if (bound%2 == 0):
-                        pred = pred - bound*200 
-                    else :
-                        pred = 200 - (pred - 200*bound)
-                elif (bound < 0) :
-                    if bound%2 ==1:
-                        pred = abs(pred - (bound+1) *200)
-                    else :
-                        pred = pred + (abs(bound)*200)
-            else : # 球正在向上 # ball goes up
-                pred = 100
-        feature.append(pred)
-        
-        feature = np.array(feature)
-        feature = feature.reshape((-1,5))
-        # 3.2. If the game is over or passed, the game process will reset
-        #      the scene and wait for ml process doing resetting job.
-        if scene_info["status"] != "GAME_ALIVE":
-            # Do some stuff if needed
-            ball_served = False
-
-            # 3.2.1. Inform the game process that ml process is ready
-            comm.ml_ready()
-            continue
-
-        # 3.3. Put the code here to handle the scene information
-
-        # 3.4. Send the instruction for this frame to the game process
-        if not ball_served:
-            if random.randrange(0,2) == 0:
-                comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_LEFT"})
+    def update(self, scene_info):
+        """
+        Generate the command according to the received scene information
+        """
+        bluecar = [];
+        self.car_pos = scene_info[self.player]
+        isBreak = False
+        isRight = True
+        isLeft = True
+        for car in scene_info["cars_info"]:
+            
+            if car["id"]==self.player_no:
+                self.car_vel = car["velocity"]
             else:
-                comm.send_to_game({"frame": scene_info["frame"], "command": "SERVE_TO_RIGHT"})
-            ball_served = True
+                bluecar.append([car["pos"],car["velocity"]])
+
+        if len(self.car_pos) == 0:
+            return []
+        for bcar in bluecar:
+            if self.car_pos[1] > bcar[0][1] and self.car_pos[1] - bcar[0][1] < 100 + (self.car_vel - bcar[1])*20 and abs(self.car_pos[0] - bcar[0][0]) < 40:
+                isBreak = True;
+               # print(self.car_pos,bcar)
+            if abs(self.car_pos[1] - bcar[0][1]) < 120:
+                if self.car_pos[0] - bcar[0][0] > 0 and self.car_pos[0] - bcar[0][0] < 71:
+                    isLeft = False
+                if self.car_pos[0] - bcar[0][0] < 0 and self.car_pos[0] - bcar[0][0] > -71:
+                    isRight = False
+        comm = []
+        if scene_info["status"] != "ALIVE":
+            return "RESET"
+        if self.direction != -1:
+            if self.direction > self.car_pos[0] and isRight:
+                comm.append("MOVE_RIGHT")
+            elif self.direction < self.car_pos[0] and isLeft:
+                comm.append("MOVE_LEFT")
+            else:
+                self.direction = -1
+        if isBreak:
+            if self.direction == -1:
+                if isRight and self.car_pos[0] < 570:
+                    if self.direction == -1:
+                        self.direction = self.car_pos[0] + 70
+                    elif self.direction < self.car_pos[0]:
+                        self.direction = self.direction + 140
+                    return ["BRAKE", "MOVE_RIGHT"]
+                elif isLeft and self.car_pos[0] > 70:
+                    if self.direction == -1:
+                        self.direction = self.car_pos[0] - 70
+                    elif self.direction > self.car_pos[0]:
+                        self.direction = self.direction - 140
+                    return ["BRAKE", "MOVE_LEFT"]
+                else:
+                    comm.append("BRAKE")
+                return comm
+            else:
+                comm.append("BRAKE")
+                return comm
         else:
-            y = clf_p1.predict(feature)
-            if y == 0:
-                comm.send_to_game({"frame": scene_info["frame"], "command": "NONE"})
-                print('NONE')
-            elif y == 1:
-                comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_LEFT"})
-                print('LEFT')
-            elif y == 2:
-                comm.send_to_game({"frame": scene_info["frame"], "command": "MOVE_RIGHT"})
-                print('RIGHT')
+            comm.append("SPEED")
+            return comm
+
+
+    def reset(self):
+        """
+        Reset the status
+        """
+        pass
